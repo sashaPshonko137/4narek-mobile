862ef6c4b7078d79b5d94888b7777cc03bb25fc2
sasik137 <privetnitron@gmail.com>
2025-02-22 17:07:02 +0500

    1

--- sasha.js
+++ sasha.js
@@ -4,7 +4,7 @@ const { join } = require('path'); // Импортируем join для рабо
 // Массив с ботами
 const bots = [
     { username: 'divers4nt404', password: 'ggggg', anarchy: 605, type: 'unbreak', inventoryPort: 3000 },
-    // { username: 'platochek225', password: 'ggggg', anarchy: 605, type: 'chorus', inventoryPort: 3001 },
+    { username: 'omnitrahyi', password: 'ggggg', anarchy: 605, type: 'chorus', inventoryPort: 3001 },
 ];
 
 // Функция для запуска Worker'ов
