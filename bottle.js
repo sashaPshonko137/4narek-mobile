1d737c9be0040d30bd835bd67273efcc04b3cea3
sasik137 <privetnitron@gmail.com>
2025-02-26 00:11:51 +0500

    112

--- bottle.js
+++ bottle.js
@@ -8,8 +8,8 @@ const minDelay = 500;
 const AHDelay = 2000;
 const loadingDelay = 100;
 
-const maxPrice = 130000
-const priceSell = 180000
+const maxPrice = 150000
+const priceSell = 200000
 
 const minBalance = 5000000
 
