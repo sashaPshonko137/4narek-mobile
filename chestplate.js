f769b6b044867760d1606de37304c19a1194f1a0
sasik137 <privetnitron@gmail.com>
2025-02-27 19:01:04 +0500

    gftcrhnt tf fgtjyvgiuky igyuiygkuighbjub

--- chestplate.js
+++ chestplate.js
@@ -162,9 +162,7 @@ async function launchBookBuyer(name, password, anarchy, inventoryPort) {
 
                 break;
 
-            case setSectionFood:
-                fs.writeFile('nether.json', JSON.stringify(bot.inventory.slots, null, 2), null)
-                
+            case setSectionFood:                
                 logger.info(`${name} - ${bot.menu}`);
                 bot.menu = sectionFood;
 
