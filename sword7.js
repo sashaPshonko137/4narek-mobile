512d35aba3c8aeaa7da7e03cc73f29a8786fb31a
sasik137 <privetnitron@gmail.com>
2025-03-06 14:46:39 +0500

    uyfg kujb

--- sword7.js
+++ sword7.js
@@ -50,11 +50,11 @@ const itemPrices = [    {
             "lvl": 1
         },
     ],
-    "priceBuy": 2000000,
-    "priceSell": 2500000
+    "priceBuy": 2500000,
+    "priceSell": 3000000
 }]
 
-const priceSell = 2500000
+const priceSell = 3000000
 
 const minBalance = 25000000
 
