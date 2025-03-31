697437117fbd35d27f24e592a3c41b15f64f3528
sasik137 <privetnitron@gmail.com>
2025-03-31 16:54:50 +0500

    hjgfhjgf

--- chorus.js
+++ chorus.js
@@ -9,8 +9,8 @@ const minDelay = 500;
 const AHDelay = 2000;
 const loadingDelay = 100;
 
-const maxPrice = 200000
-const priceSell = 400000
+const maxPrice = 150000
+const priceSell = 300000
 
 const minBalance = 5000000
 
