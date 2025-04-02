ecc364a130956428febd3c6dd35708e6a10e4953
sasik137 <privetnitron@gmail.com>
2025-04-02 16:51:29 +0500

    fdsafsd fdvdfsafdsfa356

--- helmet.js
+++ helmet.js
@@ -52,11 +52,11 @@ const itemPrices = [    {
             "lvl": 1
         },
     ],
-    "priceBuy": 1200000,
-    "priceSell": 1500000
+    "priceBuy": 1000000,
+    "priceSell": 1300000
 }]
 
-const priceSell = 1500000
+const priceSell = 1300000
 
 const minBalance = 20000000
 
