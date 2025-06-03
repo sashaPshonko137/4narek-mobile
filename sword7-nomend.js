4d7f46b8016e1939aefe7e96890efb7c3367bcfc
sashaPshonko137 <privetnitron@gmail.com>
2025-06-03 22:07:25 +0500

    klmfsadlk nklasdflk0-32-032jkndsfkj32

--- sword7-nomend.js
+++ sword7-nomend.js
@@ -51,10 +51,10 @@ const itemPrices = [    {
             "lvl": 1
         },
     ],
-    "priceBuy": 1700000,
+    "priceBuy": 1900000,
 }]
 
-const priceSell = 2100000
+const priceSell = 2300000
 
 const minBalance = 25000000
 
