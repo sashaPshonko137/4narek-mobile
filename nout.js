b42e3e33ba47bd7644e6adc220762a3b962e60fe
sashaPshonko137 <privetnitron@gmail.com>
2025-06-08 19:27:58 +0500

    lkj;fsda jksbladf9032hjksdf9032332

--- nout.js
+++ nout.js
@@ -19,12 +19,13 @@ const tgBot = new TelegramBot(token, { polling: true });
 
 // Массив с ботами
 const bots = [
-    { username: 'mr_gazoliz', password: 'ggggg', anarchy: 604, type: 'sword7', inventoryPort: 3000, balance: undefined, msgID: 0, msgTime: null, isRunning: false, isManualStop: false  },
+    { username: 'mr_gazoliz', password: 'ggggg', anarchy: 604, type: 'sword7', inventoryPort: 3000, balance: undefined, msgID: 0, msgTime: null, isManualStop: false  },
     { username: 'dudkaCringe', password: 'ggggg', anarchy: 604, type: 'sword-nomend', inventoryPort: 3001, balance: undefined, msgID: 0, msgTime: null, isRunning: false, isManualStop: false  },
     { username: 'gorbatyi_nahui', password: 'ggggg', anarchy: 604, type: 'sword', inventoryPort: 3001, balance: undefined, msgID: 0, msgTime: null, isRunning: false, isManualStop: false  },
 ];
 
-// Массив для хранения ссылок на воркеров gorbatyi_nahui
+// Массив для хранения ссылок на воркеров
+// Массив для хранения ссылок на воркеров
 let workers = [];
 
 function runWorker(bot) {
