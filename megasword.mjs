4856be9fe1f17007e1bdfdefa124180d8238a46f
sashaPshonko137 <privetnitron@gmail.com>
2025-07-22 21:29:55 +0500

    sssdddsssaaaxxxsss

--- megasword.mjs
+++ megasword.mjs
@@ -432,7 +432,7 @@ async function launchBookBuyer(name, password, anarchy, inventoryPort) {
             case myItems:
                 logger.info(`${name} - ${bot.menu}`);
                 bot.count = 0
-                for (let i = 0; i < 9; i++) {
+                for (let i = 0; i < 3; i++) {
                     if (bot.currentWindow?.slots[i]) bot.count++
                 }
                 bot.menu = setAH;
@@ -481,7 +481,14 @@ async function launchBookBuyer(name, password, anarchy, inventoryPort) {
 
         if (messageText.includes('[☃] У Вас купили')) {
             bot.ahFull = false;
-            bot.count--
+            const priceString = messageText.replace(/\D/g, '');
+            const price = parseInt(priceString);
+            const id = getIdBySellPrice(itemPrices, price)
+            if (id) {
+                await sendSell(id);
+            } else {
+                logger.error('НЕ НАШЕЛ ПРОДАННЫЙ ТОВАР БАЛЯ')
+            }
             await sellItems(bot)
             return
         }
@@ -569,7 +576,7 @@ async function launchBookBuyer(name, password, anarchy, inventoryPort) {
 
 async function sendSell(text) {
   try {
-    await fetch('http://31.207.74.231:8080/sell', {
+    await fetch('http://31.207.74.231:8080/sell_shue', {
       method: 'POST',
       body: JSON.stringify({ type: text }), // Отправляем как JSON с полем type
       headers: {
@@ -583,7 +590,7 @@ async function sendSell(text) {
 
 async function sendBuy(text) {
   try {
-    await fetch('http://31.207.74.231:8080/buy', {
+    await fetch('http://31.207.74.231:8080/buy_shue', {
       method: 'POST',
       body: JSON.stringify({ type: text }), // Отправляем как JSON с полем type
       headers: {
@@ -628,7 +635,7 @@ async function sellItems(bot) {
         // Продажа предметов только если AH не заполнен
         if (!bot.ahFull) {
             // 1. Сначала проверяем быструю панель (горячие слоты)
-            for (let quickSlot = 0; quickSlot < 0-bot.count; quickSlot++) {
+            for (let quickSlot = 0; quickSlot < 9; quickSlot++) {
                 if (bot.ahFull) break;
                 
                 const slotIndex = firstSellSlot + quickSlot;
@@ -717,7 +724,7 @@ async function sellItems(bot) {
  * @param {Array} itemPrices - Конфиг с шаблонами цен.
  * @returns {number} Цена продажи (или 0, если предмет не подходит под конфиг).
  */
-async function getBestSellPrice(item, itemPrices) {
+function getBestSellPrice(item, itemPrices) {
     // if (!item || !itemPrices?.length) return 0;
 
     // Сортируем конфиг по priceSell (от большего к меньшему)
@@ -754,7 +761,6 @@ async function getBestSellPrice(item, itemPrices) {
         }
 
         // 2. Нашли подходящий шаблон — возвращаем его priceSell!
-        await sendSell(configItem.id)
         return configItem.priceSell;
     }
 
