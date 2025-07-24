7c4b4f8d6b044c9a4d0308fc35ec64bb548a0f7d
sashaPshonko137 <privetnitron@gmail.com>
2025-07-24 19:16:53 +0500

    hjjh hjhj hjhj bb jvvj

--- megasword.mjs
+++ megasword.mjs
@@ -142,7 +142,7 @@ const itemPrices = [
     "effects": [
         {
             "name": "minecraft:unbreaking",
-            "lvl": 4
+            "lvl": 5
         },
         {
             "name": "minecraft:sharpness",
