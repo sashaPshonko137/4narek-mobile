ed862e3637a2569574b72fba63be7d055ae99bec
sashaPshonko137 <privetnitron@gmail.com>
2025-08-02 01:56:54 +0500

    лорывалоравыиолрмсч ьчстмиыв

--- megasword.mjs
+++ megasword.mjs
@@ -56,8 +56,8 @@ const itemPrices = [
             "lvl": 5
         },
     ],
-    "priceBuy": 1100000,
-    "priceSell": 1600000,
+    "priceBuy": 1200000,
+    "priceSell": 1700000,
     },
     {
     "name": "netherite_sword",
