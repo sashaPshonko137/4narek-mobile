3eab7201be678f68d56917dbaae7b56e53566632
sashaPshonko137 <privetnitron@gmail.com>
2025-08-09 13:12:41 +0500

    ировыфлиорлвфы

--- elytra.mjs
+++ elytra.mjs
@@ -44,8 +44,8 @@ const itemPrices = [
     "id": "elytra",
     "effects": [
     ],
-    "priceBuy": 1000000,
-    "priceSell": 1400000,
+    "priceBuy": 900000,
+    "priceSell": 1300000,
     },
     {
     "name": "elytra",
