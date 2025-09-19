fae3598143b984b3f78a669d13bdd41a294b792b
sashaPshonko137 <privetnitron@gmail.com>
2025-09-19 17:18:08 +0500

    gffgfdgfdgfdgfd

--- 4narek.mjs
+++ 4narek.mjs
@@ -828,7 +828,7 @@ async function getBestAHSlot(bot, itemPrices) {
 
             try {
                 const price = await getBuyPrice(slotData);
-                if (!price || price >= configItem.priceSell - 300000) continue;
+                if (!price || price >= configItem.priceSell - 400000) continue;
                 if (!configItem.priceSell) {
                     console.log(itemPrices)
                     continue
